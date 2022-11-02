acfa66f4134593df60e8c635e77a7947cc1d2519
Nongnooch Roongpiboonsopit <nongnoochr@gmail.com>
2022-11-02 23:46:56 +0700

    Add settings for server debugging (django)

--- manage.py
+++ manage.py
@@ -4,6 +4,19 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webodm.settings")
+    
+    # ----------------
+    # (NR-11/2/22) To enable debugging in vscode
+    # See https://testdriven.io/blog/django-debugging-vs-code/
+    from django.conf import settings
+
+    if settings.DEBUG:
+        if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+            import debugpy
+            debugpy.listen(("0.0.0.0", 5678))
+            print('Attached!')
+    # ----------------
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
